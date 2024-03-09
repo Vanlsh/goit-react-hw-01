26d26b34cb8196b4d48633c366196bad3a782460
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-03-09 18:16:35 +0200

    first task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+// import css from "./Profile.module.css";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="User avatar" width={200} height={200} />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  image: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
+
+export default Profile;
